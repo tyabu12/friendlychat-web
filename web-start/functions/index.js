30271d89ecc01abbe5744b4938b59fc123e172aa
Tomohito YABU <tomotomo.y.1212@gmail.com>
2019-02-24 15:15:22 +0900

    Add addWelcomeMessages function.

--- web-start/functions/index.js
+++ web-start/functions/index.js
@@ -22,7 +22,24 @@ const admin = require("firebase-admin");
 // Initialize the Firebase Admin SDK.
 admin.initializeApp();
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async user => {
+  console.log("A new user signed in for the first time.");
+  const fullName = user.displayName || "Anonymous";
+
+  // Saves the new welcome message into the database
+  // which then displays it in the FrindlyChat clients.
+  await admin
+    .firestore()
+    .collection("messages")
+    .add({
+      name: "Firebase Bot",
+      profilePicUrl: "/images/firebase-logo.png", // Firebase logo
+      text: `${fullName} signed in for the first time! Welcome!`,
+      timestamp: admin.firestore.FieldValue.serverTimestamp()
+    });
+  console.log("Welcome message written to database.");
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
